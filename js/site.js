594a084551be894b3b853ef8db6180d99526a57e
Jeff B <jeff.breuninger@gmail.com>
2022-01-21 22:09:23 -0500

    Init working

--- js/site.js
+++ js/site.js
@@ -1,24 +1,51 @@
 const cards = [5, 6, 7, 8];
-
-appendCard = (h, card) => {
-    var hand = h == "d" ? "#txtDealer" : "#txtPlayer";
-    var temp = $(hand).val();
-    $(hand).val(temp + " " + card);
-    doSum(h, card);
-};
-
-doSum = (h, card) => {
-    var hand = h == "d" ? "#txtDealerTotal" : "#txtPlayerTotal";
-    var sum = $(hand).val();
-    sum += card;
-    $(hand).val(sum);
-};
+var playerCards = [];
+var dealerCards = [];
 
 deal = () => {
-    var sum = 0;
-
+    clear();
     for (var i = 0; i < 4; i++) {
-        var hand = i == 1 || i == 3 ? "p" : "d";
-        appendCard(hand, cards[i]);
+        if (i == 1 || i == 3) {
+            dealerCards.push(cards[i]);
+        } else {
+            playerCards.push(cards[i]);
+        }
     }
+
+    display();
+};
+
+display = () => {
+    var temp = "";
+    for (var i = 0; i < dealerCards.length; i++) {
+        temp += dealerCards[i] + " ";
+    }
+    $("#txtDealer").val(temp);
+    temp = "";
+
+    for (var i = 0; i < playerCards.length; i++) {
+        temp += playerCards[i] + " ";
+    }
+    $("#txtPlayer").val(temp);
+
+    doSum();
+};
+
+doSum = () => {
+    var d = dealerCards.reduce(function (a, b) {
+        return a + b;
+    });
+    $("#txtDealerTotal").val(d);
+
+    var p = playerCards.reduce(function (a, b) {
+        return a + b;
+    });
+    $("#txtPlayerTotal").val(p);
+};
+
+clear = () => {
+    dealerCards.length = 0;
+    playerCards.length = 0;
+    $("txtDealer").val("");
+    $("txtPlayer").val("");
 };
